c905a3802a4fe5e6d605284f187a759ab79a65d7
Sergiy Sanin <sanin_sergiy@yahoo.com>
2026-01-09 07:08:41 +0000

    ++sepolia support

--- src/rainbowKitConfig.tsx
+++ src/rainbowKitConfig.tsx
@@ -1,11 +1,11 @@
 "use client"
 
 import { getDefaultConfig } from "@rainbow-me/rainbowkit";
-import { anvil, zksync, mainnet } from "wagmi/chains";
+import { sepolia, zksync, mainnet } from "wagmi/chains";
 
 export default getDefaultConfig({
     appName: "TSender",
     projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID!,
-    chains: [anvil, zksync, mainnet],
+    chains: [sepolia, zksync, mainnet],
     ssr: false
 });
